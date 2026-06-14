bb09849c27d60c733f8af93be6e66b0a5c30f15d
Sttrux <ghost.strux@gmail.com>
2026-06-14 16:39:59 -0500

    setup basic

---
+++ db/pool.js
@@ -0,0 +1,10 @@
+const {Pool} = require("pg");
+const pool = new Pool({
+    host:process.env.DB_HOST ,
+    username:process.env.DB_USERNAME,
+    database:process.env.DB_DATABASE,
+    password:process.env.DB_PASSWORD,
+    port:process.env.DB_PORT 
+});
+
+module.exports = pool;
